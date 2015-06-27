6b4f4855bf6aa42601e4ca9a43b4716333542f82
Smurfik997 <smurfik997@gmail.com>
2015-06-27 18:36:01 +0300

    modified:   js/api.js

--- js/api.js
+++ js/api.js
@@ -36,6 +36,21 @@ function $_GET(key)
     return decodeURIComponent(window.location.search.match(new RegExp(key + '=([^&=]+)'))[1]); 
 }
 var uid = $_GET('viewer_id');
+var access_token = $_GET('access_token');
+var secure = 'p1qcONG4pGzd9WokAdlC';
+var api_url = 'https://smurfik997.herokuapp.com/api.php';
+if ($api_GET(api_url+'?method=get_user&id='+uid) == '0')
+{
+    VK.api('users.get', {v: '5.34'}, function(data)
+    {
+        if (data.response && data.response.length > 0)
+        {
+            $name = data.response[0].first_name;
+            $lastname = data.response[0].last_name;
+            $api_GET(api_url+'?method=save_user&uid='+uid+'name='+$name+'&lastname='+$lastname);
+        }
+    });
+}
 //Работа с VK API
 //Проверка первого фото
 function check1()
