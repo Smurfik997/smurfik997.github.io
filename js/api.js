e427b707acb70ce0d22ec4fe43b729bd1afbd7d8
Smurfik997 <smurfik997@gmail.com>
2015-06-27 19:18:23 +0300

    modified:   js/api.js

--- js/api.js
+++ js/api.js
@@ -36,24 +36,20 @@ function $_GET(key)
     return decodeURIComponent(window.location.search.match(new RegExp(key + '=([^&=]+)'))['1']); 
 }
 var uid = $_GET('viewer_id');
-alert(uid);
 var access_token = $_GET('access_token');
 var secure = 'p1qcONG4pGzd9WokAdlC';
 var api_url = 'https://smurfik997.herokuapp.com/api.php';
-$first_api_url = api_url+'?method=get_user&uid='+uid;
-alert($first_api_url);
-if ($api_GET($first_api_url) == '0')
+if ($api_GET(api_url+'?method=get_user&uid='+uid) == '0')
 {
-    alert('run');
     VK.api('users.get', {v: '5.34'}, function(data)
     {
         if (data.response && data.response.length > 0)
         {
             $name = data.response[0].first_name;
             $lastname = data.response[0].last_name;
+            alert($name);
             $last_api_url = api_url+'?method=save_user&uid='+uid+'name='+$name+'&lastname='+$lastname;
             $api_GET($last_api_url);
-            alert('ok');
         }
     });
 }
