7aef3dcca3fc9337322c72566895a91598c916d6
Smurfik997 <smurfik997@gmail.com>
2015-12-05 09:34:25 +0200

    Commit

--- vk-app/js/api.js
+++ vk-app/js/api.js
@@ -1,4 +1,25 @@
 function loaded()
 {
+    function $_GET(key) 
+    {  
+        return decodeURIComponent(window.location.search.match(new RegExp(key + '=([^&=]+)'))['1']); 
+    }
+    
+    if (window.location.hash != "")
+    {
+        hash = window.location.hash.replace(new RegExp("#","g"),"");
+        window.location.href = window.location.protocol+"//"+window.location.hostname+"/?"+hash;
+    } else {
+        var access_token = $_GET('access_token');
+        var user_id = $_GET('user_id');
+    
+        if (access_token == "")
+        {
+            window.location.href = "https://oauth.vk.com/authorize?client_id=5175651&display=page&callback=auth&redirect_uri=https://smurfik997.github.io/vk-app/&revoke=1&response_type=token&v=5.40&scope=friends,wall,email,offline";
+        } else {
+            document.write(access_token);       
+        }
+    }
+    
     alert(document.getElementById('iframe').contentWindow.location.href);
 }
