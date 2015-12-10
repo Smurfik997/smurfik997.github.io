bc4e2b4094bdcc89ba95df26b35474b85b61b0ff
Smurfik997 <smurfik997@gmail.com>
2015-12-10 20:50:36 +0200

    Commit

--- vk-app/js/api.js
+++ vk-app/js/api.js
@@ -12,12 +12,7 @@ function $_GET(key)
     return key_req[0];
 }
 
-function click()
-{
-   alert("test");
-}
-
-   /*for (var i=1; i<20; i++)
+for (var i=1; i<20; i++)
    {
         VK.Api.call('fave.getPosts', {offset: 98, count: 1, extended: 0, access_token: $_GET('access_token')}, function(r)
         { 
@@ -26,15 +21,4 @@ function click()
                 alert('Привет, ' + r.response[0].id); 
             } 
         });     
-   }*/
-   
-/*window.onload = function()
-{
-    if ($_GET('access_token') == 'null')
-    {    
-        var elem = document.getElementById("main"); 
-        elem.innerHTML = 'Error';
-    } else {
-        document.getElementById("meth").style.display = 'block';
-    }
-};*/
+   }
