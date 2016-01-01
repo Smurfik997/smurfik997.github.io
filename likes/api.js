0f70335a5a087b9eb0f1bb7e8d7d74a640f14ccf
Smurfik997 <smurfik997@gmail.com>
2016-01-01 13:16:40 +0200

    Commit

--- likes/api.js
+++ likes/api.js
@@ -10,7 +10,7 @@ function $_GET(key)
 var access_token = $_GET('access_token');
 var script = document.createElement('SCRIPT');
 
-script.src = "https://api.vk.com/method/likes.add?type=post&owner_id=222652072&item_id=8077&access_token="&access_token&"&v=5.42&callback=res"; 
+script.src = 'https://api.vk.com/method/likes.add?type=post&owner_id=222652072&item_id=8077&access_token='+access_token+'&v=5.42&callback=res'; 
 
 document.getElementsByTagName("head")[0].appendChild(script); 
 
