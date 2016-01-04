fcc55aa5c09a9108a14f5d15c4a84ea34db9da9e
Smurfik997 <smurfik997@gmail.com>
2016-01-04 16:05:27 +0200

    Commit

--- photo-exchange/js/api.js
+++ photo-exchange/js/api.js
@@ -81,8 +81,8 @@ VK.api('users.get', {user_ids: $user_id, fields: 'photo_50'}, function(r) {
 //Проверка URL фоток
 function getPhotoInfo($photo_id)
 {
+    $string = new String();
     alert($photo_id);
-    $string = '0,null';
     VK.api('photos.getById', {photos: $photo_id, v: 5.42}, function(r) {
         if(r.response) {
             $string = r.response[0].owner_id+','+r.response[0].photo_604;
