e561dae707b77f8c5f9402b60a8bec29ec5a138e
Smurfik997 <smurfik997@gmail.com>
2016-01-04 15:16:27 +0200

    commit

--- photo-exchange/js/api.js
+++ photo-exchange/js/api.js
@@ -102,11 +102,11 @@ function check1()
     if (result['owner_id'] == $user_id)
     {
         document.getElementById('checked1').src = 'images/okey.png';
-        document.getElementById('photo1_view').class = 'block_img';
+        document.getElementById('photo1_view').className = 'block_img';
         document.getElementById('photo1_view').src = result['photo_604'];
     } else {
         document.getElementById('checked1').src = 'images/fail.png';
-        document.getElementById('photo1_view').class = 'bl_none_img block_img';
+        document.getElementById('photo1_view').className = 'bl_none_img block_img';
         document.getElementById('photo1_view').src = 'images/nofoto.png';      
     }
 }
