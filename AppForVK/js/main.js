d0ffdfa4f9e026597b044f01c364e9be6776ae41
Artem <smurfik997@gmail.com>
2017-02-19 19:43:27 +0200

    Commit

--- AppForVK/js/main.js
+++ AppForVK/js/main.js
@@ -9,13 +9,13 @@ get_file('index.tpl'); //load template
 var text = document.getElementsByName('text');
 
 function resize_main() {
-    height1 = text[0].style.height;
     function resize() {
         height1 = text[0].style.height;
         text[0].style.height = text[0].scrollHeight + 'px';
         height2 = text[0].style.height;
         console.log(text[0].scrollHeight);
     }
+    resize();
     if (height1 != height2) {
         timer = setInterval(function() {
             resize();
