85e408590f17711ea4edc183a138f52bcf8fee89
Artem <smurfik997@gmail.com>
2017-02-19 20:12:27 +0200

    Commit

--- AppForVK/js/main.js
+++ AppForVK/js/main.js
@@ -12,7 +12,7 @@ function resizeArea(text_id, minHeight, maxHeight)
    var area_hidden = $(text_id + "_hidden");
    var text = '';
    area.value.replace(/[<>]/g, '_').split("\n").each( function(s) {
-           text = text + '<div>' + s.replace(/\s\s/g, ' &nbsp;') + '&nbsp;</div>'+"\n";
+    text = text + '<div>' + s.replace(/\s\s/g, ' &nbsp;') + '&nbsp;</div>'+"\n";
    } );
    area_hidden.innerHTML = text;
    var height = area_hidden.offsetHeight + 15;
