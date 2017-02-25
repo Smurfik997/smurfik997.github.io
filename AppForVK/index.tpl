b189fa40e30153c4fab805f266715f61521aba30
Artem <smurfik997@gmail.com>
2017-02-25 21:45:12 +0200

    Commit

--- AppForVK/index.tpl
+++ AppForVK/index.tpl
@@ -1,6 +1,6 @@
-<div style="display: none;" id="text_hidden"></div>
-<div class="H1">New Post</div>
-<form name="test" method="post" action="#" align="center">
- <input type="file" id="text-file" accept="text/plain" onchange="file_func(this.files);">
-</form>
-<!--text/plain-->
+<div id="main_block" class="main_block">
+    <div style="display: none;" id="text_hidden"></div>
+    <div id="h">New Post</div>
+    <label id="button" for="text-file" style="margin-top: 0; position: fixed; text-align: center;">Select Files</label>
+    <input type="file" id="text-file" accept="text/plain" style="display: none;" onchange="file_func(this.files);" > 
+</div>
--- AppForVK/js/main.js
+++ AppForVK/js/main.js
@@ -6,7 +6,6 @@ function get_file(file_name) {
 }
 get_file('index.tpl'); //load template
 
-
 function file_func(files) {
     var file = files[0];
     var reader = new FileReader();
@@ -15,3 +14,20 @@ function file_func(files) {
     };
     reader.readAsText(file);
 }
+
+function ver_align() {
+    var a = document.getElementById('main_block').clientHeight;
+    var b = document.getElementById('h').clientHeight;
+    var c = document.getElementById('text-file').clientHeight;
+    
+    document.getElementById('button').style.width = document.getElementById('button').clientWidth + 15;
+    var a1 = document.getElementById('main_block').clientWidth;
+    var b1 = document.getElementById('button').clientWidth;
+    
+    document.getElementById('button').style.marginTop = (a - b - c)/2;
+    document.getElementById('button').style.marginLeft = (a1 - b1)/2;
+}
+
+window.onload = function() {
+    ver_align();
+};
