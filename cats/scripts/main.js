70e48e4c5e3af20f11a599e6aae0a1bd5dd2cf31
Artem <smurfik997@gmail.com>
2017-08-08 22:24:44 +0300

    Commit

--- cats/scripts/main.js
+++ cats/scripts/main.js
@@ -10,6 +10,20 @@ Math.sRandom = function(min, max) {
     return Math.floor(Math.random() * (max + 1 - min)) + min;
 };
 
+function preloader() {
+    if (doc('pagePreloader') == undefined)
+    {
+        var pagePreloader = document.createElement('div');
+        var preloaderText = document.createElement('div');
+        pagePreloader.id = 'pagePreloader';
+        preloaderText.id = 'preloaderText';
+        preloaderText.innerText = 'Page is prepearing...';
+        document.body.appendChild(pagePreloader);
+        doc('pagePreloader').appendChild(preloaderText);
+        doc('mainBlock').style.display = 'none';
+    }
+}
+
 var client = document.createElement('iframe'); 
 
 client.src = 'scripts/cats.json';
@@ -25,6 +39,20 @@ client.onload = function(e) {
     doc('contentSizes').style.height = data.cats[random].height;
 
     document.body.removeChild(client);
+
+    var script = document.createElement('script');
+    script.src = 'scripts/page.js';
+    document.body.appendChild(script).onload = function() {
+        preloader();
+        
+        var cat = new Image()
+        cat.src = doc('content').getAttribute('imageURL');
+        doc('content').removeAttribute('imageURL');
+        cat.onload = function(e) {
+            doc('content').style.backgroundImage = 'url(' + cat.src + ')';
+            resize();
+        }
+    }
 }
 
 document.body.appendChild(client);
@@ -33,3 +61,7 @@ var title = 'Cats Finder';
 
 document.title = title;
 doc('pageName').innerText = title;
+
+document.body.onload = function() {
+    preloader();
+}
--- cats/scripts/page.js
+++ cats/scripts/page.js
@@ -10,24 +10,6 @@ function changeSizes(name, cssStr) {
     }
 }
 
-function preloaderAnim() {
-
-}
-
-function preloader() {
-    if (doc('pagePreloader') == undefined)
-    {
-        var pagePreloader = document.createElement('div');
-        var preloaderText = document.createElement('div');
-        pagePreloader.id = 'pagePreloader';
-        preloaderText.id = 'preloaderText';
-        preloaderText.innerText = 'Page is prepearing...';
-        document.body.appendChild(pagePreloader);
-        doc('pagePreloader').appendChild(preloaderText);
-        doc('mainBlock').style.display = 'none';
-    }
-}
-
 function resize() {
     var width = document.documentElement.clientWidth;
     var height = window.innerHeight;
@@ -114,18 +96,6 @@ function resize() {
 }
 
 document.body.onresize = function() {
-    preloader();
+    preloader()
     resize();
-};
-
-document.body.onload = function() {
-    preloader();
-
-    var cat = new Image()
-    cat.src = doc('content').getAttribute('imageURL');
-    doc('content').removeAttribute('imageURL');
-    cat.onload = function(e) {
-        doc('content').style.backgroundImage = 'url(' + cat.src + ')';
-        resize();
-    }
 };
