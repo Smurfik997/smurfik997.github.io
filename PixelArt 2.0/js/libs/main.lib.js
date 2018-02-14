843890a334e14513554faddb0303b1353a508d33
Artem <smurfik997@gmail.com>
2018-02-14 20:53:39 +0200

    PixelArt 2.0

---
+++ PixelArt 2.0/js/libs/main.lib.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//vars
+var winW, winH, docE;
+
+docE = document.documentElement;
+
+//warn
+console.warn('Script has injected');
+
+//functions
+function doc(p1, p2) {
+    return p2 == undefined? document.getElementById(p1) : document.getElementsByClassName(p1)[p2];
+}
+
+function resize() {
+    winW = docE.clientWidth;
+    winH = docE.clientHeight;
+
+    if (winH == undefined || winW == undefined) {
+        console.error('#001')
+    } else { 
+        var css = function(len) {
+            new Map([
+                ['height', len + 'px'],
+                ['width', len + 'px'],
+                ['marginLeft', (winW - len) / 2 + 'px'],
+                ['marginTop', (winH - len) / 2 + 'px']
+            ]).forEach(function(val, key) {
+                console.log(val, key);
+                doc('main').style[key] = val;
+            });
+        }
+
+        winW >= winH? css(winH * 0.9): css(winW * 0.9);
+    }
+}
+
+//events
+document.body.onresize = resize();
