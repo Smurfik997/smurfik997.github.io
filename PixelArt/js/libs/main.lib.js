71810d3532653ded702db9478cd00c29e2a1f563
Artem <smurfik997@gmail.com>
2018-03-31 16:49:53 +0300

    cssFix

--- PixelArt/js/libs/main.lib.js
+++ PixelArt/js/libs/main.lib.js
@@ -94,8 +94,11 @@ function resize() {
                 }
             })
 
-            var fCurrNumW = Math.trunc(winH * .072 * 3.5)
+            var fCurrNumW = Math.trunc(window.innerHeight * .072 * 3.2)
             doc('fCurrNum').querySelector('div.counter').style.width = fCurrNumW
+            doc('fCurrNum').style.margin = window.innerHeight * .024 + 'px 0'
+            doc('fCurrNum').style.lineHeight = window.innerHeight * .072 + 'px'
+            doc('fCurrNum').style.fontSize = window.innerHeight * .04 + 'px'
             doc('fCurrNum').querySelector('div.counter').style.marginLeft = Math.trunc((len * .7 - fCurrNumW) / 2)
             doc('createM').style.fontSize = len * .06 + 'px'
             doc('createM').querySelectorAll('div.counter').forEach((e) => {
