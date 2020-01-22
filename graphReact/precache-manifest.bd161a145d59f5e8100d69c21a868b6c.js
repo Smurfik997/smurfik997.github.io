6907f0c3a47c107905f0f0bae8234c2b78132650
Artem <smurfik997@gmail.com>
2020-01-23 00:50:45 +0200

    Graph Build 1.0

---
+++ graphReact/precache-manifest.bd161a145d59f5e8100d69c21a868b6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba216875a755d11aa94e0a9aa14ffec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "897ecd49764251c3a72a",
+    "url": "/static/js/2.9dd58185.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9dd58185.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e8d9785783f323e41ce",
+    "url": "/static/js/main.6447987a.chunk.js"
+  },
+  {
+    "revision": "c67e7fc494555f284ce1",
+    "url": "/static/js/runtime-main.a43b6eb2.js"
+  }
+]);
