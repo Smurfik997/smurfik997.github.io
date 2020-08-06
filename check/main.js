81b3982b6b1f9b516cd31171b8829c5acba7d57c
Artem <smurfik997@gmail.com>
2020-08-07 02:21:01 +0300

    new styles

--- check/main.js
+++ check/main.js
@@ -7,8 +7,10 @@ loginCallback = (res) => {
         document.location.href = 'profile.html'
     } else {
         document.getElementById('error-block').innerText = res.ERR.uk
-        document.getElementById('error-block').setAttribute('data-visible', '')
-        setTimeout(() => document.getElementById('error-block').removeAttribute('data-visible'), 1500)
+        if (document.getElementById('error-block').getAttribute('data-visible') == null) {
+            document.getElementById('error-block').setAttribute('data-visible', '')
+            setTimeout(() => document.getElementById('error-block').removeAttribute('data-visible'), 1000)
+        }
     }
 }
 
@@ -33,8 +35,10 @@ apiRequest = (method, params, callback) => {
         document.body.removeChild(js)
         document.forms['login-form'].removeAttribute('data-loading')
         document.getElementById('error-block').innerText = 'Сервер недоступний'
-        document.getElementById('error-block').setAttribute('data-visible', '')
-        setTimeout(() => document.getElementById('error-block').removeAttribute('data-visible'), 1500)
+        if (document.getElementById('error-block').getAttribute('data-visible') == null) {
+            document.getElementById('error-block').setAttribute('data-visible', '')
+            setTimeout(() => document.getElementById('error-block').removeAttribute('data-visible'), 1000)
+        }
     }
     document.body.appendChild(js)
 }
@@ -53,3 +57,11 @@ logout = () => {
     document.cookie = 'apikey=; max-age=0'
     document.location.href = 'index.html'
 }
+
+// datalist = document.createElement('datalist')
+// datalist.id = 'products'
+// document.forms['login-form'].appendChild(datalist)
+// option = document.createElement('option')
+// option.value = 'KU'
+// datalist.appendChild(option)
+// option.value = 'LOL'
